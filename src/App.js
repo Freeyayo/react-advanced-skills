b07c12c2f3226d247b027071196d848308e0d1b0
Freeyayo <caiconghao@icloud.com>
2021-07-24 22:34:38 +0800

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import React, { useState } from 'react';
 // import StateMachine from './comps/StateMachine';
 // import StateMachine2 from './comps/StateMachine2';
 // import CompoundComps from './comps/CompoundComps'
-import CompoundComps2 from './comps/CompoundComps2'
+// import CompoundComps2 from './comps/CompoundComps2'
+import CompoundComps3 from './comps/CompoundComps3'
 
 import './style.css';
 
 export default function App() {
-  const commonProps = {
-    color: 'red',
-    increment: 2
-  };
   return (
     <div>
-      <CompoundComps2 />
+      <CompoundComps3 />
     </div>
   );
 }
